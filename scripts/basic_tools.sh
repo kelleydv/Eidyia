ece0b55065eb784eef5f7ba9fe1656430a37be8d
steamlight <kelleydv@gmail.com>
2013-11-05 15:39:03 -0800

    added curl

--- scripts/basic_tools.sh
+++ scripts/basic_tools.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
+#!/bin/bash 
 sudo apt-get -y install build-essential # g++, make, etc.
-sudo apt-get -y install git 
-# configure git
-
+sudo apt-get -y install git
+sudo apt-get -y install curl
 sudo apt-get -y install x11-apps
