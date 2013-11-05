7164fe895c29f36cf2626dba8475bcddb5f86eb3
steamlight <kelleydv@gmail.com>
2013-11-05 09:30:38 -0800

    initial commit

---
+++ scripts/basic_tools.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get -y install build-essential # g++, make, etc.
+sudo apt-get -y install git 
+# configure git
+
+sudo apt-get -y install x11-aps
---
+++ scripts/python_etc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get -y install python3-matplotlib #installs python3 as a dependency
+sudo apt-get -y install python3-nose
+#sudo apt-get -y install python3-scipy
+#sudo apt-get -y install ipython3-notebook
+
